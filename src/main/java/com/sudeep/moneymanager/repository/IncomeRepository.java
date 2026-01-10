8841e48e72fb4d5c1e9cb63d1b2e9f7dda25dc09
sudeep <sahusudeepkumar3@gmail.com>
2026-01-10 10:43:18 +0530

    income and expense (create, get, delete)

---
+++ src/main/java/com/sudeep/moneymanager/repository/IncomeRepository.java
@@ -0,0 +1,40 @@
+package com.sudeep.moneymanager.repository;
+
+import com.sudeep.moneymanager.entity.IncomeEntity;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IncomeRepository extends JpaRepository<IncomeEntity, Long> {
+
+    //select * from tbl_incomes where profile_id = ? order by date desc
+    List<IncomeEntity> findByProfileIdOrderByDateDesc(Long profileId);
+
+    //select * from tbl_incomes where profile_id = ? order by date desc limit 5
+    List<IncomeEntity> findTop5ByProfileIdOrderByDateDesc(Long profileId);
+
+
+    // it is a jpql query not a finder method
+    @Query("SELECT SUM(i.amount) FROM IncomeEntity i where i.profile.id = :profileId")
+    BigDecimal findTotalExpenseByProfileId(@Param("profileId") Long profileId);
+
+    //select * from tbl_incomes where profile_id = ? and date between ? and ? and name like %?%
+    List<IncomeEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(
+            Long profileId,
+            LocalDate startDate,
+            LocalDate endDate,
+            String keyword,
+            Sort sort
+
+    );
+
+    //select * from tbl_incomes where profile_id = ? and date between ? and ?
+    List<IncomeEntity> findByProfileIdAndDateBetween(Long profileId,LocalDate startDate,LocalDate endDate);
+
+
+}
---
+++ src/main/java/com/sudeep/moneymanager/service/ExpenseService.java
@@ -0,0 +1,99 @@
+package com.sudeep.moneymanager.service;
+
+import com.sudeep.moneymanager.dto.ExpenseDTO;
+import com.sudeep.moneymanager.entity.CategoryEntity;
+import com.sudeep.moneymanager.entity.ExpenseEntity;
+import com.sudeep.moneymanager.entity.ProfileEntity;
+import com.sudeep.moneymanager.repository.CategoryRepository;
+import com.sudeep.moneymanager.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+    private final CategoryRepository categoryRepository;
+    private final ExpenseRepository expenseRepository;
+    private final ProfileService profileService;
+
+    //add a new expense to the database
+    public ExpenseDTO addExpense(ExpenseDTO dto){
+    ProfileEntity profile = profileService.getCurrentProfile();
+    CategoryEntity category = categoryRepository.findById(dto.getCategoryId())
+        .orElseThrow(()->new RuntimeException("Category not found"));
+    ExpenseEntity newExpense = toEntity(dto,profile,category);
+    newExpense = expenseRepository.save(newExpense);
+    return toDTO(newExpense);
+    }
+
+
+    //retrieves all expenses for the current month/based on the start and end date
+     public List<ExpenseDTO> getCurrentMonthExpensesForCurrentUser(){
+        ProfileEntity profile = profileService.getCurrentProfile();
+         LocalDate now = LocalDate.now();
+         LocalDate startDate = now.withDayOfMonth(1);
+         LocalDate endDate = now.withDayOfMonth(now.lengthOfMonth());
+         List<ExpenseEntity> list = expenseRepository.findByProfileIdAndDateBetween(profile.getId(),startDate,endDate);
+         return list.stream().map(this::toDTO).toList();
+
+     }
+
+     //delete expenses by id for current user
+    public void deleteExpenses(Long expenseId){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        ExpenseEntity entity = expenseRepository.findById(expenseId)
+                .orElseThrow(()-> new RuntimeException("Expense not found"));
+        if(!entity.getProfile().getId().equals(profile.getId())){
+            throw new RuntimeException("Unauthorized to delete this expense");
+        }
+        expenseRepository.delete(entity);
+
+    }
+
+    //get latest 5 expenses for current user
+    public List<ExpenseDTO> getLatest5ExpensesForCurrentUser(){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<ExpenseEntity> list = expenseRepository.findTop5ByProfileIdOrderByDateDesc(profile.getId());
+        return list.stream().map(this::toDTO).toList();
+    }
+
+    //get total expenses for current user
+    public BigDecimal getTotalExpenseForCurrentUser(){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        BigDecimal total = expenseRepository.findTotalExpenseByProfileId(profile.getId());
+        return total != null ? total : BigDecimal.ZERO;
+    }
+
+
+
+
+    //helper method
+    private ExpenseEntity toEntity(ExpenseDTO dto, ProfileEntity profile, CategoryEntity category){
+        return ExpenseEntity.builder()
+                .name(dto.getName())
+                .icon(dto.getIcon())
+                .amount(dto.getAmount())
+                .date(dto.getDate())
+                .profile(profile)
+                .category(category)
+                .build();
+    }
+
+    private ExpenseDTO toDTO(ExpenseEntity entity){
+       return ExpenseDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .categoryId(entity.getCategory()!= null ? entity.getCategory().getId():null)
+                .categoryName(entity.getCategory()!= null? entity.getCategory().getName():"N/A")
+                .amount(entity.getAmount())
+                .date(entity.getDate())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+}
