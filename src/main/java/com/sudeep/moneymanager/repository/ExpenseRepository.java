ba36d8a56a0a7b5679d10ee504447e92d272da19
sudeep <sahusudeepkumar3@gmail.com>
2026-01-18 10:39:51 +0530

    daily email notification

--- src/main/java/com/sudeep/moneymanager/repository/ExpenseRepository.java
+++ src/main/java/com/sudeep/moneymanager/repository/ExpenseRepository.java
@@ -36,4 +36,7 @@ Sort sort
     //select * from tbl_expenses where profile_id = ? and date between ? and ?
   List<ExpenseEntity> findByProfileIdAndDateBetween(Long profileId,LocalDate startDate,LocalDate endDate);
 
+
+  List<ExpenseEntity> findByProfileIdAndDate(Long profileId, LocalDate date);
+
 }
