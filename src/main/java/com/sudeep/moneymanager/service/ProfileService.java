c581e92292781266af79b7577c499c3f857055dd
sudeep <sahusudeepkumar3@gmail.com>
2026-01-21 14:50:02 +0530

    docker file

--- src/main/java/com/sudeep/moneymanager/service/ProfileService.java
+++ src/main/java/com/sudeep/moneymanager/service/ProfileService.java
@@ -6,6 +6,7 @@ import com.sudeep.moneymanager.entity.ProfileEntity;
 import com.sudeep.moneymanager.repository.ProfileRepository;
 import com.sudeep.moneymanager.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -26,6 +27,9 @@ public class ProfileService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+
+    @Value("${app.activation.url}")
+    private String activationURL;
    public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile = toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
@@ -33,7 +37,7 @@ public class ProfileService {
 
 
         //send activation email
-       String activationLink = "http://localhost:8080/api/v1.0/activate?token="+newProfile.getActivationToken();
+       String activationLink = activationURL+"/api/v1.0/activate?token="+newProfile.getActivationToken();
        String subject = "Activate your money manager account";
        String body = "Click on the following link to activate your account: "+activationLink;
        emailService.sendEmail(newProfile.getEmail(),subject,body);
